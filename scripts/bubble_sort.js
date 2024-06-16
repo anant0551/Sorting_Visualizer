860cd85d20fb8503fbd330bd8f2caa0bd96b04c8
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-06-16 18:49:38 +0530

    Update bubble_sort.js

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -6,39 +6,39 @@
 
 function Bubble()
 {
-    //Setting Time Complexities
+    //Setting Time complexities
     document.getElementById("Time_Worst").innerText="O(N^2)";
-    document.getElementById("Time_Average").innerText="⊖(N^2)";
+    document.getElementById("Time_Average").innerText="Θ(N^2)";
     document.getElementById("Time_Best").innerText="Ω(N)";
-    
-    //Setting Space Complexity
+
+    //Setting Space complexity
     document.getElementById("Space_Worst").innerText="O(1)";
 
     c_delay=0;
 
-    for (var i=0;i<array_size-1;i++)
+    for(var i=0;i<array_size-1;i++)
+    {
+        for(var j=0;j<array_size-i-1;j++)
         {
-            for (var j=0;j<array_size-i-1;j++)
-                {
-                    div_update(divs[j],div_sizes[j],"yellow");//color Update
+            div_update(divs[j],div_sizes[j],"yellow");//Color update
 
-                    if(div_sizes[j]>div_sizes[j+1])
-                        {
-                            div_update(divs[j],div_sizes[j],"red");//color Update
-                            div_update(divs[j+1],div_sizes[j+1],"red");//color Update                                                                                                                                                                    //done by anant kumar singh
+            if(div_sizes[j]>div_sizes[j+1])
+            {
+                div_update(divs[j],div_sizes[j], "red");//Color update
+                div_update(divs[j+1],div_sizes[j+1], "red");//Color update
 
-                            var temp=div_sizes;
-                            div_sizes[j]=div_sizes[j+1];
-                            div_sizes[j+1]=temp;
+                var temp=div_sizes[j];
+                div_sizes[j]=div_sizes[j+1];
+                div_sizes[j+1]=temp;
 
-                            div_update(divs[j],div_sizes[j],"red");//Height Update
-                            div_update(divs[j+1],div_sizes[j+1],"red");//Height Update
-                        }
-                        div_update(divs[j],div_sizes[j], "blue");//color Update
-                }
-                div_update(divs[j],div_sizes[j], "green");//color Update
+                div_update(divs[j],div_sizes[j], "red");//Height update
+                div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+            }
+            div_update(divs[j],div_sizes[j], "blue");//Color updat
         }
-        div_update(divs[0],div_sizes[0], "green");//color Update
+        div_update(divs[j],div_sizes[j], "green");//Color update
+    }
+    div_update(divs[0],div_sizes[0], "green");//Color update
 
-        enable_buttons();
+    enable_buttons();
 }
