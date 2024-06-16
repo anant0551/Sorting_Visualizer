9084560bf84dcffad62c87dc92b56b02a9a5bce1
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-06-16 18:50:23 +0530

    Update heap_sort.js

--- scripts/heap_sort.js
+++ scripts/heap_sort.js
@@ -3,83 +3,96 @@
     DONE BY :- ANANT KUMAR SINGH 
     *****************************    
 */
-function Heap() {
-    //Setting Time Complexities
+function Heap()
+{
+    //Setting Time complexities
     document.getElementById("Time_Worst").innerText="O(N log N)";
-    document.getElementById("Time_Average").innerText="⊖(N log N)";
+    document.getElementById("Time_Average").innerText="Θ(N log N)";
     document.getElementById("Time_Best").innerText="Ω(N log N)";
 
-    //Setting Space Complexity
+    //Setting Space complexity
     document.getElementById("Space_Worst").innerText="O(1)";
 
     c_delay=0;
-    heap_sort();
 
+    heap_sort();
+    
     enable_buttons();
 }
 
-function swap(i,j) {
-    div_update(divs[i],div_sizes[i],"red");//color Update
-    div_update(divs[j],div_sizes[j],"red");//color Update                                                                                                                                                                    //done by anant kumar singh
+function swap(i,j)
+{
+    div_update(divs[i],div_sizes[i],"red");//Color update
+    div_update(divs[j],div_sizes[j],"red");//Color update
 
     var temp=div_sizes[i];
     div_sizes[i]=div_sizes[j];
     div_sizes[j]=temp;
 
-    div_update(divs[i],div_sizes[i],"red");//Height Update
-    div_update(divs[j],div_sizes[j],"red");//Height Update
+    div_update(divs[i],div_sizes[i],"red");//Height update
+    div_update(divs[j],div_sizes[j],"red");//Height update
 
-    div_update(divs[i],div_sizes[i],"blue");//Color Update
-    div_update(divs[j],div_sizes[j],"blue");//Color Update
+    div_update(divs[i],div_sizes[i],"blue");//Color update
+    div_update(divs[j],div_sizes[j],"blue");//Color update
 }
 
-function max_heapify(n,i) {
+function max_heapify(n,i)
+{
     var largest=i;
     var l=2*i+1;
     var r=2*i+2;
 
-
-    if (l<n && div_sizes[l]>div_sizes[largest]) {
-        if (largest!=i) {
-            div_update(divs[largest],div_sizes[largest],"blue");//color update
+    if(l<n && div_sizes[l]>div_sizes[largest])
+    {
+        if(largest!=i)
+        {
+            div_update(divs[largest],div_sizes[largest],"blue");//Color update
         }
+
         largest=l;
-        div_update(divs[largest],div_sizes[largest],"red");//color update
+
+        div_update(divs[largest],div_sizes[largest],"red");//Color update
     }
 
-
-    if (r<n && div_sizes[r]>div_sizes[largest]) {
-        if (largest!=i) {
-            div_update(divs[largest],div_sizes[largest],"blue");//color update
+    if(r<n && div_sizes[r]>div_sizes[largest])
+    {
+        if(largest!=i)
+        {
+            div_update(divs[largest],div_sizes[largest],"blue");//Color update
         }
+
         largest=r;
-        div_update(divs[largest],div_sizes[largest],"red");//color update
+
+        div_update(divs[largest],div_sizes[largest],"red");//Color update
     }
 
-    if (largest!=i) {
+    if(largest!=i)
+    {
         swap(i,largest);
 
         max_heapify(n,largest);
     }
 }
 
-function heap_sort() {
-    for (var i=Math.floor(array_size/2)-1;i>=0;i--){
-        max_heapify(array_size,i);      
+function heap_sort()
+{
+    for(var i=Math.floor(array_size/2)-1;i>=0;i--)
+    {
+        max_heapify(array_size,i);
     }
 
-    for (var i = array_size; i > 0; i--) {
-        swap(0,1);
-        div_update(divs[i],div_sizes[i],"green");//color update
-        div_update(divs[i],div_sizes[i],"yellow");//color update
+    for(var i=array_size-1;i>0;i--)
+    {
+        swap(0,i);
+        div_update(divs[i],div_sizes[i],"green");//Color update
+        div_update(divs[i],div_sizes[i],"yellow");//Color update
 
         max_heapify(i,0);
-        
-        div_update(divs[i],div_sizes[i],"blue");//color update
-        div_update(divs[i],div_sizes[i],"green");//color update
-    }
-    div_update(divs[i],div_sizes[i],"green");//color update
 
+        div_update(divs[i],div_sizes[i],"blue");//Color update
+        div_update(divs[i],div_sizes[i],"green");//Color update
+    }
+    div_update(divs[i],div_sizes[i],"green");//Color update
 }
 
 
